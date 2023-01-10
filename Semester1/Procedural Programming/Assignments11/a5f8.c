a550050f7cb76737fcd602a9468747ba36cf1a43
Mad <44483926+madskgg@users.noreply.github.com>
2023-01-10 17:56:05 +0200

    Update a5f8.c

--- Semester1/Procedural Programming/Assignments11/a5f8.c
+++ Semester1/Procedural Programming/Assignments11/a5f8.c
@@ -66,16 +66,18 @@ void calculateTotal(int num, infoT sellers[], long total[])
 
 void printData(long total[])
 {
+  int i;
+    
   printf("%11s %12s", "PERIOXH", "SYN.PWLHSEWN\n");
 
-  for (int i=0; i<24; i++) printf("-");
+  for (i=0; i<24; i++) printf("-");
 
   printf("\n%11s %12ld\n", "Thes/niki" ,total[0]);
   printf("%11s %12ld\n", "Athens", total[1]);
   printf("%11s %12ld\n", "Volos", total[2]);
   printf("%11s %12ld\n", "Hrakleio", total[3]);
 
-  for (int i=0; i<24; i++) printf("-");
+  for (i=0; i<24; i++) printf("-");
 
   printf("\n%11s %12ld", "SYNOLO:", total[0]+total[1]+total[2]+total[3]);
 }
