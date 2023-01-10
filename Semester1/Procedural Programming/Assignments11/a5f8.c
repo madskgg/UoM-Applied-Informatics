5f6ece2116a3d646f7f6e457844f8a848c462101
Mad <44483926+madskgg@users.noreply.github.com>
2023-01-10 17:53:04 +0200

    Add files via upload

---
+++ Semester1/Procedural Programming/Assignments11/a5f8.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include "simpio.h"
+
+#define N 20
+
+typedef struct
+{
+    int code;
+    char surname[16];
+    long total_sales;
+    int area;
+} infoT;
+
+void readData(int num, infoT sellers[]);
+void calculateTotal(int num, infoT sellers[], long total[]);
+void printData(long total[]);
+
+int main()
+{
+    int num;
+    long total[4]={0,0,0,0};
+    infoT sellers[N];
+
+    printf("Dwse to plhthos twn polhtwn? ");
+    num = GetInteger();
+    readData(num, sellers);
+    calculateTotal(num, sellers, total);
+    printData(total);
+    return 0;
+}
+
+
+void readData(int num, infoT sellers[])
+{
+    int i;
+
+    for (i=0;i<num;i++)
+    {
+        printf("Kwdikos? ");
+        sellers[i].code = GetInteger();
+        printf("Onomatepwnymo? ");
+        gets(sellers[i].surname);
+        printf("Poso pwlhsewn? ");
+        sellers[i].total_sales = GetReal();
+        printf("Kwdikos (1= Thes/niki, 2= Athens, 3= Volos, 4= Hrakleio)? ");
+        sellers[i].area = GetInteger();
+    }
+}
+
+void calculateTotal(int num, infoT sellers[], long total[])
+{
+    int i;
+
+    for (i=0;i<num;i++)
+    {
+        if (sellers[i].area == 1)
+            total[0]+= sellers[i].total_sales;
+        else if (sellers[i].area == 2)
+            total[1]+= sellers[i].total_sales;
+        else if (sellers[i].area == 3)
+            total[2]+= sellers[i].total_sales;
+        else
+            total[3]+= sellers[i].total_sales;
+    }
+}
+
+void printData(long total[])
+{
+  printf("%11s %12s", "PERIOXH", "SYN.PWLHSEWN\n");
+
+  for (int i=0; i<24; i++) printf("-");
+
+  printf("\n%11s %12ld\n", "Thes/niki" ,total[0]);
+  printf("%11s %12ld\n", "Athens", total[1]);
+  printf("%11s %12ld\n", "Volos", total[2]);
+  printf("%11s %12ld\n", "Hrakleio", total[3]);
+
+  for (int i=0; i<24; i++) printf("-");
+
+  printf("\n%11s %12ld", "SYNOLO:", total[0]+total[1]+total[2]+total[3]);
+}
---
+++ Semester1/Procedural Programming/Assignments11/a8f8.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include "simpio.h"
+
+typedef struct
+{
+    int width;
+    int height;
+    int depth;
+    int area;
+    int volume;
+} box;
+
+box readData(box a_box);
+void calc_box_area(box *a_box);
+void calc_box_volume(box *a_box);
+
+int main()
+{
+    box a_box;
+
+    a_box=readData(a_box);
+
+    calc_box_area(&a_box);
+    calc_box_volume(&a_box);
+
+    printf("To emvadon tou koutiou einai %d cm2\n", a_box.area);
+    printf("O ogos tou koutiou einai %d cm3\n", a_box.volume);
+
+    return 0;
+}
+
+box readData(box a_box)
+{
+	printf("Dose to mikos tou koutiou se cm: ");
+	a_box.width=GetInteger();
+	printf("Dose to ypsos tou koutiou se cm: ");
+	a_box.height=GetInteger();
+	printf("Dose to vathos tou koutiou se cm: ");
+	a_box.depth=GetInteger();
+	a_box.area=0;
+	a_box.volume=0;
+	return a_box;
+}
+
+void calc_box_area(box *a_box)
+{
+    (*a_box).area = 2*((*a_box).width * (*a_box).depth + (*a_box).width * (*a_box).height + (*a_box).depth * (*a_box).height);
+}
+
+void calc_box_volume(box *a_box)
+{
+    (*a_box).volume = (*a_box).width * (*a_box).height * (*a_box).depth;
+}
+
+/*Μπορεί εναλλακτικά να χρησιμοποιηθεί και ο παρακάτω τρόπος
+
+void calc_box_area(box *a_box)
+{
+    a_box->area = 2*(a_box->width * a_box->height + a_box->height * a_box->depth + a_box->depth * a_box->width);
+}
+
+void calc_box_volume(box *a_box)
+{
+    a_box->volume = a_box->width * a_box->height * a_box->depth;
+}
+
+*/
